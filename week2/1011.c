963636b58519a42148d713d78210bd2a0a8e226b
yeochaeeon <codjs2659@gmail.com>
2023-07-10 17:11:00 +0900

    ~1017 clear

--- week2/1011.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-char main()
-{
-  char a;
-  scanf("%c",&a);
-  printf("%c",a);
-}
--- week2/1012.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-float main()
-{
-  float a;
-  scanf("%f",&a);
-  printf("%f\n",a);
-}
