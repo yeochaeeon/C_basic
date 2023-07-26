07fb3f8ebbc917cd399283649e337b02347f5303
yeochaeeon <codjs2659@gmail.com>
2023-07-26 15:16:32 +0900

    Update codeup ~ 1068

---
+++ c_codeup/1059.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+  int a;
+  scanf("%d", &a);
+  printf("%d\n",-a-1);
+  return 0;
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+}
+
